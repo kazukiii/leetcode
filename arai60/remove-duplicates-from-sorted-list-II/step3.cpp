6f0b47e756e954af07f99dad8a37c3d99c8ef65a
kazukiii <kazumatsu.dev28@gmail.com>
2024-06-08 16:17:38 -0700

    step1, step2, step3を追加

---
+++ arai60/remove-duplicates-from-sorted-list-II/step3.cpp
@@ -0,0 +1,35 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* deleteDuplicates(ListNode* head) {
+        ListNode dummy;
+        dummy.next = head;
+
+        ListNode* prev = &dummy;
+        ListNode* curr = head;
+        while (curr) {
+            while (curr->next && curr->val == curr->next->val) {
+                curr = curr->next;
+            }
+
+            if (prev->next != curr) {
+                prev->next = curr->next;
+            } else {
+                prev = curr;
+            }
+
+            curr = curr->next;
+        }
+
+        return dummy.next;
+    }
+};
